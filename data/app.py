cf92c3fcffaa0cedf770ea6ed81116eb5ee0fbc9
vishnunadendla <vishnunadendla9949@gmail.com>
2026-05-10 16:07:42 -0400

    Errors were fixed

--- data/app.py
+++ data/app.py
@@ -176,7 +176,7 @@ col1, col2 = st.columns(2)
 
 with col1:
     if st.button("💼 Work experience?"):
-        question = "What is Vishnu's work experience?"
+        question = "How many of years experinece vihsnu have and what are they"
 
 with col2:
     if st.button("🛠️ Technical skills?"):
